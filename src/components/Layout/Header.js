2e4b5109b3dd25b5a4613b5dd704547323260bd7
Raunaq singh <macbook@Raunaqs-MacBook-Air.local>
2022-06-27 16:08:06 +0530

    Create CartButton component for Header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,13 +3,14 @@ import React from "react";
 import imgMeals from "../../assets/Images/meals.jpg";
 
 import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 function Header() {
   return (
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button></button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={imgMeals} alt="Table full of delicious meals!" />
